9879ececa44536e2967c717122ee387cc71cffdc
Reimar <mail@reim.ar>
2024-08-13 19:06:43 +0200

    Implement releasing rust backend

---
+++ release.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd API
+dotnet publish -r linux-arm64 -p:PublishSingleFile=true --self-contained false
+dotnet ef migrations bundle -r linux-arm64 --force
+
+cd ../rust-backend
+cargo build --release --target=aarch64-unknown-linux-gnu
+
+cd ..
+rsync -va \
+	rust-backend/target/aarch64-unknown-linux-gnu/release/skantravels \
+	API/efbundle \
+	API/bin/Release/net8.0/linux-arm64/publish/API \
+	API/bin/Release/net8.0/linux-arm64/publish/libe_sqlite3.so \
+	reimar@reim.ar:/home/reimar/skantravels
+
+ssh -t reimar@reim.ar "systemctl restart skantravels && systemctl restart skantravels-rust"
+
