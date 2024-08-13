1ba6d82c0a5f4780e64ffd6531b7cd0adfd8e63e
Reimar <mail@reim.ar>
2024-08-13 10:12:18 +0200

    Add database migrations to release script

--- API/release.sh
+++ API/release.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+
 dotnet publish -r linux-arm64 -p:PublishSingleFile=true --self-contained false
-rsync -vu bin/Release/net8.0/linux-arm64/publish/* reimar@reim.ar:/home/reimar/skantravels
+dotnet ef migrations bundle --self-contained -r linux-arm64 --force
+
+rsync -va bin/Release/net8.0/linux-arm64/publish/* reimar@reim.ar:/home/reimar/skantravels
+rsync -va efbundle reimar@reim.ar:/home/reimar/skantravels
+
 ssh -t reimar@reim.ar "systemctl restart skantravels"
 
