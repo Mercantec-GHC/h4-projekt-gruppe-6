7552197fc63223953c4386b6ab89b2e010541c96
Reimar <mail@reim.ar>
2024-08-13 14:37:13 +0200

    Add database migrations to rust backend

--- rust-backend/src/main.rs
+++ rust-backend/src/main.rs
@@ -1,5 +1,10 @@
 use actix_web::{get, Responder, HttpResponse, HttpServer, App};
 
+mod embedded {
+    use refinery::embed_migrations;
+    embed_migrations!("./migrations");
+}
+
 #[get("/hc")]
 async fn healthcheck() -> impl Responder {
     HttpResponse::Ok().body("OK")
@@ -12,11 +17,22 @@ async fn main() -> std::io::Result<()> {
         .and_then(|port| port.parse::<u16>().ok())
         .unwrap_or(8080);
 
+    let database_path = std::env::var("RUST_BACKEND_DB")
+        .unwrap_or("database.sqlite3".to_string());
+
+    println!("Opening database: {}", database_path);
+
+    let mut conn = rusqlite::Connection::open(database_path).unwrap();
+
+    embedded::migrations::runner().run(&mut conn).unwrap();
+
+    println!("Starting web server at port {}", port);
+
     HttpServer::new(|| {
         App::new()
             .service(healthcheck)
     })
-    .bind(("127.0.0.1", port))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
