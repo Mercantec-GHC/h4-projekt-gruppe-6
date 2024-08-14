21f7a16d98ba3105f823677cc8f8dc7bff17645f
Alexandertp <sandertp@gmail.com>
2024-08-14 12:36:05 +0200

    Fix https redirection, also send authorization header to delete request

--- API/integration-tests.sh
+++ API/integration-tests.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
-RESPONSE=`curl -sSX POST http://localhost:5287/api/Users -d '{"Email":"hej@example.com","Username":"Gamer","Password":"Gamer123!"}' --header 'Content-Type: application/json' --write-out '\n%{http_code}' --output -`
+
+RESPONSE=`curl -sSLX POST http://localhost:5287/api/Users -d '{"Email":"hej@example.com","Username":"Gamer","Password":"Gamer123!"}' -H 'Content-Type: application/json' -H 'Accept: */*' -w '\n%{http_code}' --output -`
 HTTP_STATUS=`echo -n "$RESPONSE" | tail -n 1`
 USER_ID=`echo "$RESPONSE" | head -n -1`
 
 echo "POST /api/Users - $HTTP_STATUS"
 echo -e "  User ID: $USER_ID\n"
 
-RESPONSE=`curl -sSX POST http://localhost:5287/api/Users/login -d '{"Email":"hej@example.com","Password":"Gamer123!"}' --header 'Content-Type: application/json' --write-out '\n%{http_code}' --output -`
+RESPONSE=`curl -sSLX POST http://localhost:5287/api/Users/login -d '{"Email":"hej@example.com","Password":"Gamer123!"}' -H 'Content-Type: application/json' -H 'Accept: */*'  -w '\n%{http_code}' --output -`
 HTTP_STATUS=`echo -n "$RESPONSE" | tail -n 1`
 TOKEN=`echo "$RESPONSE" | head -n -1`
 
 echo "POST /api/Users/login - $HTTP_STATUS"
 echo -e "  Received token: $TOKEN\n"
 
-RESPONSE=`curl -sS http://localhost:5287/api/Users/$USER_ID --write-out '\n%{http_code}' --output -`
+RESPONSE=`curl -sSL http://localhost:5287/api/Users/$USER_ID -w '\n%{http_code}' -H 'Accept: */*' --output -`
 HTTP_STATUS=`echo -n "$RESPONSE" | tail -n 1`
 USER_DATA=`echo "$RESPONSE" | head -n -1`
 
 echo "GET /api/Users/$USER_ID - $HTTP_STATUS"
 echo -e "  User data: $USER_DATA\n"
 
-HTTP_STATUS=`curl -sSX DELETE http://localhost:5287/api/Users/$USER_ID --write-out '%{http_code}'`
+HTTP_STATUS=`curl -sSLX DELETE http://localhost:5287/api/Users/$USER_ID --location-trusted -w '%{http_code}' -H "Authorization: Bearer $TOKEN" -H 'Accept: */*'`
 echo "DELETE /api/Users/$USER_ID - $HTTP_STATUS"
 
