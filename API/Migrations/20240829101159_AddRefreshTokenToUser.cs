ffb5e20f79e423701122842c598b694d9ea7d3ba
Sandertp <sandertp@gmail.com>
2024-08-29 12:16:01 +0200

    Add Database Migration for RefreshToken in Users
    Co-authored-by: Reimar <mail@reim.ar>

---
+++ API/Migrations/20240829101159_AddRefreshTokenToUser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiresAt",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiresAt",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
--- API/Models/User.cs
+++ API/Models/User.cs
@@ -7,6 +7,8 @@ public class User : BaseModel
     public string? Email { get; set; }
     public string? Username { get; set; }
     public string HashedPassword { get; set; }
+    public string RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiresAt { get; set; }
 }
 
 public class UserDTO
